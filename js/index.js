81aba2c603bf25634ec2aa9e541c7cd35b886cc7
Michelle Moon <michellemoon236@gmail.com>
2019-07-29 01:41:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
+<<<<<<< HEAD
   let userName = document.getElementById('username').value
   req.open('GET', `https://api.github.com/users/${userName}/repos`);
+=======
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+>>>>>>> 18ef556e64933d782d1c6bd08648acd1f65b7535
   req.send();
 }
 
@@ -10,15 +14,23 @@ function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
+<<<<<<< HEAD
     .map( 
+=======
+    .map(
+>>>>>>> 18ef556e64933d782d1c6bd08648acd1f65b7535
       r =>
         '<li>' +
         r.name +
         ' - <a href="#" data-repo="' +
         r.name +
+<<<<<<< HEAD
         '" onclick="getCommits(this)">Get Commits</a></li>' +
         r.name +
         '" onclick="getBranches(this)">Get Branches</a></li>'
+=======
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+>>>>>>> 18ef556e64933d782d1c6bd08648acd1f65b7535
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
@@ -28,8 +40,12 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
+<<<<<<< HEAD
   let userName = document.getElementById('username').value
   req.open('GET', `https://api.github.com/repos/${userName}/` + name + '/commits');
+=======
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+>>>>>>> 18ef556e64933d782d1c6bd08648acd1f65b7535
   req.send();
 }
 
